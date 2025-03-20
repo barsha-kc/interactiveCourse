89597579ecaee932d45a5f62706094690c31485b
barsha-kc <barshakc182@gmail.com>
2025-03-20 17:37:15 +0200

    Tic tac toe

---
+++ hw5/app1/src/components/Square.jsx
@@ -0,0 +1,14 @@
+function Square({ value }) {
+  function handleClick() {
+    console.log('clicked!');
+  }
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
