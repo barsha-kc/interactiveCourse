a454fd0a05c54ea8a2c5412c7e7082057f10dd2c
barsha-kc <barshakc182@gmail.com>
2025-03-20 17:48:01 +0200

    tic tac toe winner

--- hw5/app1/src/App.jsx
+++ hw5/app1/src/App.jsx
@@ -26,6 +26,8 @@ export default function Board() {
     setXisNext(true);
   }
 
+  const winner = calculateWinner(squares);
+  let status = winner ? `Winner: ${winner}`;
   
 
   return (
@@ -45,6 +47,15 @@ export default function Board() {
         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
+
+      function calculateWinner(squares){
+        const winningLines = [
+          [0, 1, 2], [3, 4, 5], [6, 7, 8], 
+          [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+          [0, 4, 8], [2, 4, 6] 
+        ];
+      
+      }
 
 
       <div className="reset-container">
